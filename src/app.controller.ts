51872f41b74047c137a86769def57da6ba4918fc
cuevacelis <cuevacelis@hotmail.com>
2025-06-05 10:18:29 -0500

    feat: enhance API documentation and add global prefix

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,11 +1,29 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('app')
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
+  @ApiOperation({ summary: 'Endpoint de salud de la aplicación' })
+  @ApiResponse({
+    status: 200,
+    description:
+      'Retorna un mensaje de confirmación de que la API está funcionando',
+    schema: {
+      type: 'object',
+      properties: {
+        message: {
+          type: 'string',
+          example: 'Hello World!',
+          description: 'Mensaje de confirmación',
+        },
+      },
+    },
+  })
   getHello(): string {
     return this.appService.getHello();
   }
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,32 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common'; // Importa ValidationPipe
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Configurar el prefijo global para todas las rutas
+  app.setGlobalPrefix('dev/v1', {
+    exclude: ['/'], // Excluir la ruta raíz si es necesario
+  });
+
+  // Configuración de Swagger
+  const config = new DocumentBuilder()
+    .setTitle('Tenpo API')
+    .setDescription(
+      'API de Tenpo para gestión de beneficios y tarjetas de crédito',
+    )
+    .setVersion('1.0')
+    .addTag('benefits', 'Endpoints relacionados con beneficios')
+    .addTag('credit-card', 'Endpoints relacionados con tarjetas de crédito')
+    .addTag('summary', 'Endpoints relacionados con resúmenes financieros')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, document);
+
   app.useGlobalPipes(
     new ValidationPipe({
       transform: true, // Habilita la transformación de tipos (ej. string a number)
